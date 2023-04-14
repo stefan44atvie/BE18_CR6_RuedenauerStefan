c401e038fe82c923cbc34f7fd06321f288b53b17
stefan44atvie <107038049+stefan44atvie@users.noreply.github.com>
2023-04-14 10:24:48 +0200

    some changes

--- src/Form/EventsType.php
+++ src/Form/EventsType.php
@@ -12,16 +12,16 @@ class EventsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('event_name')
+            ->add('event_name', NULL, ["attr" => ["placeholder" => "Please enter the name of the upcoming event", "class" => "eventname form-control w-50"]])
             ->add('event_datetime')
-            ->add('event_description')
+            ->add('event_description', NULL, ["attr" => ["placeholder" => "Please enter a description for that event", "class" => "description form-control w-50"]])
             ->add('event_image')
             ->add('event_capacity')
-            ->add('event_email')
-            ->add('event_phone')
-            ->add('event_address')
-            ->add('event_url')
-            ->add('event_type')
+            ->add('event_email', NULL, ["attr" => ["placeholder" => "Please enter an email", "class" => "email form-control w-50"]])
+            ->add('event_phone', NULL, ["attr" => ["placeholder" => "Please enter phone number", "class" => "phone form-control w-50"]])
+            ->add('event_address', NULL, ["attr" => ["placeholder" => "Please enter an address", "class" => "address form-control w-50"]])
+            ->add('event_url', NULL, ["attr" => ["placeholder" => "Please enter URL", "class" => "event_url form-control w-50"]])
+            ->add('event_type', NULL, ["attr" => ["placeholder" => "Please enter event-type", "class" => "event_type form-control w-50"]])
         ;
     }
 

bb192c36b95dce7e800a05402ca4a91eaf9d8ed0
stefan44atvie <107038049+stefan44atvie@users.noreply.github.com>
2023-04-14 10:41:58 +0200

    image url

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/events')]
+#[Route('/')]
 class EventsController extends AbstractController
 {
     #[Route('/', name: 'app_events_index', methods: ['GET'])]
